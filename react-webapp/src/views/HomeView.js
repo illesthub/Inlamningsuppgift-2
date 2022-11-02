163347da7516e0ee0b8766607c2ed3f54dad9d30
Harald Mörk Jonsson <morkjonssonharald@gmail.com>
2022-11-02 01:06:26 +0100

    ProductCard added

--- react-webapp/src/views/HomeView.js
+++ react-webapp/src/views/HomeView.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainMenuSection from '../sections/MainMenuSection'
 import FooterSection from '../sections/FooterSection'
+import ProductGridSection from '../sections/ProductGridSection'
 
 const Home = () => {
   return (
     <>
       <MainMenuSection />
+      <ProductGridSection />
       <FooterSection />
     </>
 
