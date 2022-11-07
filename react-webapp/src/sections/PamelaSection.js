4abe0f5c9ad344e45f4a0c398d63fb06069ef9d5
Harald Mörk Jonsson <morkjonssonharald@gmail.com>
2022-11-07 01:03:03 +0100

    Pamela finished

--- react-webapp/src/sections/PamelaSection.js
+++ react-webapp/src/sections/PamelaSection.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import PamelasPicks from "../assets/images/pamela-picks-image.png"
+import LetsBeConscious from "../assets/images/lets-be-conscious.png"
+import { NavLink } from 'react-router-dom'
 
 const PamelaSection = () => {
   return (
-    <section>
-        <div className='container'>
-            <div className='pamelas-picks'></div>
-            <div className='lets-be-conscious'></div>
+    <section className='pamela-section'>
+        <div className='container-fluid'>
+            <div className='pamelas-picks'>
+              <img src={PamelasPicks} />
+              <h1>Pamela Reif's <span>Top Picks</span></h1>
+              <NavLink className='button btn-theme-dark'>                    
+                    <span className='corner-left'></span>
+                    <span className='corner-right'></span>
+                    SHOP NOW
+              </NavLink>
+            </div>
+            <div className='lets-be-conscious'>
+              <img src={LetsBeConscious} />
+              <h1>Let's Be <span>Conscious</span></h1>
+              <NavLink className='button btn-theme-light'>                    
+                    <span className='corner-left'></span>
+                    <span className='corner-right'></span>
+                    FLASH SALE
+              </NavLink>
+            </div>
         </div>
     </section>
   )
